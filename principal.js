6bc10afe0e6f544cbdab3b53b0cc0abec6fdb269
Alyson Henrike <101460518+aly16br@users.noreply.github.com>
2022-11-10 10:36:06 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,6 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricioonista";
+
+var paciente = document.querySelector("#paciente-um");
+
+console.log("paciente");
