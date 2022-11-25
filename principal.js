26d882552c42d41794d22ff220e3fd167d92b05e
Alyson Henrike <101460518+aly16br@users.noreply.github.com>
2022-11-25 09:24:58 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -3,4 +3,29 @@ titulo.textContent = "Aparecida Nutricioonista";
 
 var paciente = document.querySelector("#paciente-um");
 
-console.log("paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var PesoEhValido = true;
+var AlturaEhValida = true;
+
+
+if (peso < 0 || peso > 1000){
+	console.log("Peso Inválido!");
+	PesoEhValido = false;
+}
+
+if (altura < 0 || altura > 3.00) {
+	console.log("Altura Inválida!");
+	AlturaEhValida = false;
+}
+
+if (PesoEhValido && AlturaEhValida) {
+	var imc = peso / (altura * altura);
+	tdImc.textContent = imc;
+}
